aa2f62375a7a9f5cb4ab7208f4ee966abdbe5520
Gopichand003-tech <chenapalligopichand@gmail.com>
2025-09-08 15:30:47 +0530

    refactor: load Google Client ID from .env instead of hardcoding

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,8 +5,7 @@ import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { AuthProvider } from "./context/AuthContext";
 
-const CLIENT_ID =
-  "101083858594-t1j5pu3ioib1971udtik03lbhdun0kdh.apps.googleusercontent.com";
+const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
